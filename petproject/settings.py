cd7ced8e628cc9f337ca2c6aec7ed1f3c97f4e8d
aneeshabasheer <aneeshabasheer1997@gmail.com>
2026-07-16 09:34:49 +0530

    procfile added

--- petproject/settings.py
+++ petproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4oba9r12$9x94o%@@2ftmtuxq013wl+mcbq+dvv3$^)eh-2)zr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
